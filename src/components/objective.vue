d3f60fba63883160c5ba0a7c7315c8e152c83f7b
iYuan <yuan@iyuan.ltd>
2022-12-02 07:11:48 +0800

    夜间模式初步适配完成 (#25)
    * 夜间模式初步适配完成
    
    夜间模式初步适配完成
    
    * 漏网之鱼
    
    Co-authored-by: hissin <hissin@126.com>

--- src/components/objective.vue
+++ src/components/objective.vue
@@ -6,12 +6,11 @@ export default {
       objectData: [],
       objectLoaded: null,
       errorRate: 0.3,
-      objectFullData: []
-    }
+      objectFullData: [],
+    };
   },
   methods: {
     randomObject() {
-
       let randomRank = function (n, min, max) {
         let a = [];
         let sum = max - min + 1;
@@ -24,28 +23,27 @@ export default {
             }
           }
         }
-        a.sort((a, b) => a - b)
+        a.sort((a, b) => a - b);
         return a;
-      }
+      };
 
       let result = [...this.objectData];
       randomRank(Math.trunc(this.objectData.length * this.errorRate), 1, this.objectData.length).forEach((tea) => {
         tea = tea - 1;
         if (result[tea] == 'A') {
-          result[tea] = Math.random() > 0.5 ? 'B' : 'C'
+          result[tea] = Math.random() > 0.5 ? 'B' : 'C';
         } else if (result[tea] == 'B') {
-          result[tea] = Math.random() > 0.5 ? 'A' : 'D'
+          result[tea] = Math.random() > 0.5 ? 'A' : 'D';
         } else if (result[tea] == 'C') {
-          result[tea] = Math.random() > 0.5 ? 'A' : 'D'
+          result[tea] = Math.random() > 0.5 ? 'A' : 'D';
         } else if (result[tea] == 'D') {
-          result[tea] = Math.random() > 0.5 ? 'B' : 'C'
+          result[tea] = Math.random() > 0.5 ? 'B' : 'C';
         } else {
-          result[tea] = Math.random() > 0.5 ? Math.random() > 0.5 ? 'B' : 'C' : Math.random() > 0.5 ? 'A' : 'D'
+          result[tea] = Math.random() > 0.5 ? (Math.random() > 0.5 ? 'B' : 'C') : Math.random() > 0.5 ? 'A' : 'D';
         }
-
-      })
+      });
       this.objectData = result;
-    }
+    },
   },
   watch: {
     userInfo() {
@@ -56,77 +54,73 @@ export default {
         },
         body: this.Qs.stringify({
           token: this.userInfo.token,
-          taskId: this.taskInfo.taskId
-        })
-      }).then(
-        res => res.json()
-      ).then(res => {
-        if (res.state == 'ok') {
-          this.objectData = [];
-          res.data.forEach((tea) => {
-            if (tea.hasSubjectiveItem == 0) {
-              this.objectData.push(tea.teaAnswer)
-              //this.$bus.$emit('object-num', this.objectData.length)
-            }
-          })
-          this.objectLoaded = 2;
-
-        } else if (res.state == 'over') {
-          this.logout()
-        } else {
-
-          //取回选择题数据
-          fetch(`https://service-cz1sw5t1-1301539318.bj.apigw.tencentcs.com/release/?userrole=${this.userInfo.userRole}&schoolid=${this.userInfo.schoolId}&sid=${this.taskInfo.taskId}&token=${this.userInfo.token}&userid=${this.userInfo.userId}&username=${this.userInfo.realName}&schoolname=${this.userInfo.schoolName}&type=query`)
-            .then(
-              res => res.json()
-            ).then(res => {
-              //console.log(res.code == 1 || !res.answer);
-              if (res.code == 0 && res.answer) {
-                this.objectData = res.answer.replace(/[0-9]+/g, "").replace(/\. /g, "").split('\n');
-                this.objectLoaded = 2
-              }
-              else if (res.code == 1 || !res.answer) {
-                for (let i of this.taskData) {
-                  if (i.hasSubjectiveItem == 0) {
-                    this.objectData.push("C");
-                  }
-                }
-                this.objectLoaded = 3
-              }
-
-            }).catch(err => {
-              console.log(err);
-            });
-
-        }
-      }).catch(err => {
-        console.log(err);
+          taskId: this.taskInfo.taskId,
+        }),
       })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.state == 'ok') {
+            this.objectData = [];
+            res.data.forEach((tea) => {
+              if (tea.hasSubjectiveItem == 0) {
+                this.objectData.push(tea.teaAnswer);
+                //this.$bus.$emit('object-num', this.objectData.length)
+              }
+            });
+            this.objectLoaded = 2;
+          } else if (res.state == 'over') {
+            this.logout();
+          } else {
+            //取回选择题数据
+            fetch(`https://service-cz1sw5t1-1301539318.bj.apigw.tencentcs.com/release/?userrole=${this.userInfo.userRole}&schoolid=${this.userInfo.schoolId}&sid=${this.taskInfo.taskId}&token=${this.userInfo.token}&userid=${this.userInfo.userId}&username=${this.userInfo.realName}&schoolname=${this.userInfo.schoolName}&type=query`)
+              .then((res) => res.json())
+              .then((res) => {
+                //console.log(res.code == 1 || !res.answer);
+                if (res.code == 0 && res.answer) {
+                  this.objectData = res.answer
+                    .replace(/[0-9]+/g, '')
+                    .replace(/\. /g, '')
+                    .split('\n');
+                  this.objectLoaded = 2;
+                } else if (res.code == 1 || !res.answer) {
+                  for (let i of this.taskData) {
+                    if (i.hasSubjectiveItem == 0) {
+                      this.objectData.push('C');
+                    }
+                  }
+                  this.objectLoaded = 3;
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    taskData(){
-
+    taskData() {
       for (let i = 0; i < this.taskData.length; i++) {
         let tea = this.taskData[i];
         if (tea.hasSubjectiveItem == 0) {
-          this.objectFullData.push(tea)
+          this.objectFullData.push(tea);
         }
       }
-    }
+    },
   },
   created() {
     this.objectLoaded = 1;
   },
-}
+};
 </script>
 
-
-
 <template>
-  <div class="bg-blue-200  shadow overflow-hidden rounded-lg mt-4" v-if="objectNum > 0">
-    <div class="text-lg leading-8 font-bold text-blue-700 flex px-4 py-4 sm:px-6">
+  <div class="bg-blue-200 dark:bg-blue-700 shadow overflow-hidden rounded-lg mt-4" v-if="objectNum > 0">
+    <div class="text-lg leading-8 font-bold text-blue-700 dark:text-white flex px-4 py-4 sm:px-6">
       <div class="w-1/2">客观题</div>
       <label class="input-group justify-end w-1/2">
-        <select class="select select-secondary select-sm" v-model="this.errorRate">
+        <select class="select select-secondary select-sm dark:bg-zinc-800" v-model="this.errorRate">
           <option value="0.1">错10%</option>
           <option value="0.2">错20%</option>
           <option value="0.3" selected>错30%</option>
@@ -139,36 +133,27 @@ export default {
     </div>
     <div class="border-blue-200 rounded-lg">
       <dl>
-        <div class="bg-white bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2 " v-if="this.objectLoaded == 2">
+        <div class="bg-white dark:bg-zinc-700 bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2" v-if="this.objectLoaded == 2">
           <div class="indicator mx-4 mb-4 mt-5" v-for="(tea, index) in this.objectData" :key="index">
             <span class="indicator-item badge badge-secondary indicator-start text-white font-bold">{{ index + 1 }}</span>
-            <input type="text" class="input input-secondary w-16 pl-2 pr-0 py-0 uppercase"
-              onkeyup="var start = this.selectionStart;  var end = this.selectionEnd;  this.value = this.value.toUpperCase();  this.setSelectionRange(start, end);"
-              v-model="this.objectData[index]" />
+            <input type="text" class="input input-secondary w-16 pl-2 pr-0 py-0 uppercase dark:bg-zinc-800 dark:text-white" onkeyup="var start = this.selectionStart;  var end = this.selectionEnd;  this.value = this.value.toUpperCase();  this.setSelectionRange(start, end);" v-model="this.objectData[index]" />
           </div>
         </div>
 
-        <div class="bg-white bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2" v-if="this.objectLoaded == 1">
+        <div class="bg-white dark:bg-zinc-700 bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2 dark:text-white" v-if="this.objectLoaded == 1">
           <button class="btn btn-ghost loading">获取中...</button>
         </div>
 
-
-        <div class="bg-white bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2" v-if="this.objectLoaded == 3">
+        <div class="bg-white dark:bg-zinc-700 bg-opacity-90 px-4 sm:gap-4 sm:px-6 rounded-lg py-2" v-if="this.objectLoaded == 3">
           <div class="flex my-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-              class="stroke-info flex-shrink-0 w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
             <span class="text-blue-500 font-bold">我们暂未获得此套作业答案,请稍后重试[前人用小鑫助手提交过此套作业才能获取答案],已为您填充C。</span>
           </div>
           <div class="indicator mx-4 mb-4 mt-5" v-for="(tea, index) in this.objectFullData" :key="index">
-            <span class="indicator-item badge badge-secondary indicator-start text-white font-bold">{{
-                tea.teaCode
-            }}</span>
-            <input type="text" class="input input-secondary w-16 pl-2 pr-0 py-0 uppercase"
-              v-model="this.objectData[index]"
-              onkeyup="var start = this.selectionStart;  var end = this.selectionEnd;  this.value = this.value.toUpperCase();  this.setSelectionRange(start, end);"/>
+            <span class="indicator-item badge badge-secondary indicator-start text-white font-bold">{{ tea.teaCode }}</span>
+            <input type="text" class="input input-secondary w-16 pl-2 pr-0 py-0 uppercase dark:bg-zinc-800 dark:text-white" v-model="this.objectData[index]" onkeyup="var start = this.selectionStart;  var end = this.selectionEnd;  this.value = this.value.toUpperCase();  this.setSelectionRange(start, end);" />
           </div>
         </div>
       </dl>
